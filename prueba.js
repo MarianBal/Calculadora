3fcc88440281ecffe480f2bd13e540931f7b2abb
Mariana Bálsamo <44822014+MarianBal@users.noreply.github.com>
2019-01-18 21:10:08 -0300

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,34 @@
+var numero1 = prompt('Ingresa un número','');
+numero1 = parseInt(numero1);
+var operacion = prompt('Ingrese la operacion', '');
+var numero2 = prompt('Ingresa un número','');
+numero2 = parseInt(numero2);
+var resultado;
+
+if(!isNaN(numero1) && !isNaN(numero2)) {
+
+switch (operacion) {
+    case '+':
+    resultado = numero1 + numero2;
+    break;
+
+    case '-':
+    resultado = numero1 - numero2;
+    break;
+
+    case '*':
+    resultado = numero1 * numero2;
+    break;
+
+    case '/':
+    resultado = numero1 / numero2;
+    break;
+    // Completar…
+    default:document.write('falta un dato');
+    }
+    if (typeof(resultado) !='undefined') {
+        document.write(resultado);
+    } else {
+        document.write('Ingrese un numero');
+    }
+}
